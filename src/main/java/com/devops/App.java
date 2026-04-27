94165dbdf2ace83e5c19fb789496cf5948483958
manjot <man@gmail.com>
2026-04-27 17:00:34 +0530

    Initial commit - Maven Build automation project is uploading on git

---
+++ src/main/java/com/devops/App.java
@@ -0,0 +1,8 @@
+package com.devops;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Manjot! Welcome to DevOps i");
+	System.out.println("Hello Manjot! Maven Build Successful ");
+    }
+}
